e0955e80913d32aba380d93f2217759e7803ab0e
Daddaabdelghafour <99355457+Daddaabdelghafour@users.noreply.github.com>
2025-12-05 17:06:00 +0100

    Specify LibraryManagementApplication in tests
    Updated Spring Boot test to specify application class.

--- src/test/java/org/example/tp3pipeline/Tp3PipelineApplicationTests.java
+++ src/test/java/org/example/tp3pipeline/Tp3PipelineApplicationTests.java
@@ -2,12 +2,14 @@ package org.example.tp3pipeline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.example.tp3pipeline.library.LibraryManagementApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryManagementApplication.class)
 class Tp3PipelineApplicationTests {
 
     @Test
     void contextLoads() {
+        // Ce test vérifie juste que Spring Boot arrive à démarrer
     }
 
 }
